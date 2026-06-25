c303e0f207ddb8ba367d3913a11e15be946586e1
Natique Ibrar <ibrarnatique@gmail.com>
2026-06-26 01:42:32 +0400

    Added test db, error in test user still left to fix

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,15 +1,50 @@
 from fastapi.testclient import TestClient
+from fastapi import Depends
+import os
+import sqlite3
+import pytest
+
 from app.main import app
 from app.schemas import UserOut
 
+from app.config import settings
+from app.db_util import get_db
 
-client = TestClient(app)
 
-"""
-def test_create_user(): res = client.post(
+TEST_DB_PATH = os.path.join(
+    "C:\\Users\\ibrar\\OneDrive\\Desktop\\Api\\Post-Api\\app",
+    f"test_{settings.db_name}",
+)
+
+
+def override_get_db():
+    conn = sqlite3.connect(TEST_DB_PATH)
+    conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
+    try:
+        yield conn
+
+    finally:
+        conn.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture
+def client(connection: sqlite3.Connection = Depends(override_get_db)):
+    curr = connection.cursor()
+    curr.execute("DELETE FROM users")
+    yield TestClient(app)
+    curr.execute("DELETE FROM users")
+    connection.commit()
+
+
+def test_create_user(client):
+    res = client.post(
         "/users/", json={"email": "test_user@gmail.com", "password": "1234567Ilove"}
     )
     new_user = UserOut(**res.json())
+    # print(res.json())
     assert (res.status_code == 201) or (res.status_code == 409)
     assert new_user.email == "test_user@gmail.com"
-"""
