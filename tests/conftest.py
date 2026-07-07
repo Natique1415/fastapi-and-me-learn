62db19b50df3c3e5860fb40c2f47d79604f86c81
Natique Ibrar Alam <ibrarnatique@gmail.com>
2026-07-08 02:49:57 +0400

    Added post_test to inject sample post data in test_bd and wrote tests for that ( check test_post.py to know more)

--- tests/conftest.py
+++ tests/conftest.py
@@ -47,7 +47,7 @@ def test_new_user():
 
 
 @pytest.fixture
-def test_authorized_user(client):
+def test_authorized_user():
     authorized_user = {"email": "test_user@gmail.com", "password": "12345678"}
     conn = sqlite3.connect(TEST_DB_PATH)
     curr = conn.cursor()
@@ -62,6 +62,58 @@ def test_authorized_user(client):
     finally:
         conn.close()
         return authorized_user
+
+
+"""
+Create a fixture which inserts bunch of posts into TEST_DB with the user_id from the authorized user an then use those for further 
+tests in test_post.py
+"""
+
+
+@pytest.fixture
+def test_post(test_authorized_user):
+    conn = sqlite3.connect(TEST_DB_PATH)
+    curr = conn.cursor()
+    posts_data = [
+        {
+            "title": "1st title",
+            "content": "first content",
+            "user_id": test_authorized_user["user_id"],
+        },
+        {
+            "title": "2nd title",
+            "content": "2nd content",
+            "user_id": test_authorized_user["user_id"],
+        },
+        {
+            "title": "3rd title",
+            "content": "3rd content",
+            "user_id": test_authorized_user["user_id"],
+        },
+        {
+            "title": "3rd title",
+            "content": "3rd content",
+            "user_id": test_authorized_user["user_id"],
+        },
+    ]
+
+    try:
+        for post in posts_data:
+            curr.execute(
+                "INSERT INTO posts(title,content,user_id) VALUES(?,?,?) RETURNING id",
+                (
+                    post["title"],
+                    post["content"],
+                    post["user_id"],
+                ),
+            )
+            post_id = curr.fetchone()
+            post["post_id"] = post_id[0]
+        conn.commit()
+
+    finally:
+        conn.close()
+        return posts_data
 
 
 @pytest.fixture
--- tests/test_post.py
+++ tests/test_post.py
@@ -1,5 +1,4 @@
 from app.schemas import Post
-from pydantic import ValidationError
 import pytest
 
 
@@ -13,13 +12,21 @@ def test_get_posts(client):
     """
 
 
-def test_latest_post(client):
+def test_latest_post_when_post_present(client, test_post):
     res = client.get("/posts/latest")
-    assert res.status_code in (404, 200)
-    try:
-        latest_post = Post.model_validate(res.json())
-    except ValidationError as _:
-        ...
+    assert res.status_code == 200
+    Post.model_validate(res.json())
+
+
+def test_latest_post_when_no_post_present(client):
+    res = client.get("/posts/latest")
+    assert res.status_code == 404
+
+
+def test_get_one_not_exit_post(client):
+    res = client.get(f"/posts/{1000}")
+    assert res.status_code == 404
+    assert res.json().get("detail") == f"Given Post Id = {1000} Does not exist"
 
 
 @pytest.mark.parametrize(
@@ -45,6 +52,13 @@ def test_create_post(
     assert created_post.content == content
     assert created_post.user_id == test_authorized_user["user_id"]
     assert created_post.is_published == is_published
+    assert created_post.no_of_likes == 0
+
+
+def test_get_one_valid_post(client, test_post):
+    res = client.get(f"/posts/{test_post[0]['post_id']}")
+    assert res.status_code == 200
+    Post.model_validate(res.json())
 
 
 def test_unauthorized_create_post(client):
